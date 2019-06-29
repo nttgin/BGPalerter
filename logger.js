8e133351a43f8dec6c87e75843a90bc45f856e7a
Massimo Candela <nve@webrobotics.net>
2019-06-29 22:53:45 +0200

    added winston for logs

---
+++ logger.js
@@ -0,0 +1,13 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'reports.log' })
+    ]
+});
+
+logger.remove(logger.transports.Console);
+module.exports = logger;
