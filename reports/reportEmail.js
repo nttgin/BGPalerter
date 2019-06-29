8cf10033f7a4ca42d0e91ef82b3181f8d4858997
Massimo Candela <nve@webrobotics.net>
2019-06-29 03:47:39 +0200

    config param to disable emails

--- reports/reportEmail.js
+++ reports/reportEmail.js
@@ -6,28 +6,32 @@ export default class ReportEmail extends Report {
     constructor(channels, config, pubSub) {
         super(channels, config, pubSub);
 
-        this.transporter = nodemailer.createTransport({
-            host: this.config.emailConfig.smtp,
-            port: this.config.emailConfig.port,
-            secure: this.config.emailConfig.secure,
-            auth: {
-                user: this.config.emailConfig.user,
-                pass: this.config.emailConfig.password
-            }
-        });
+        if (this.config.emailConfig.enabled) {
+            this.transporter = nodemailer.createTransport({
+                host: this.config.emailConfig.smtp,
+                port: this.config.emailConfig.port,
+                secure: this.config.emailConfig.secure,
+                auth: {
+                    user: this.config.emailConfig.user,
+                    pass: this.config.emailConfig.password
+                }
+            });
+        }
     }
 
     report = (message, content) => {
 
-        this.transporter
-            .sendMail({
-                from: this.config.emailConfig.email,
-                to: this.config.notifiedEmails.join(', '),
-                subject: "BGP alert: " + message,
-                text: "Hello world?"
-            })
-            .catch(error => {
-                console.log("log the error properly!", error);
-            })
+        if (this.transporter) {
+            this.transporter
+                .sendMail({
+                    from: this.config.emailConfig.email,
+                    to: this.config.notifiedEmails.join(', '),
+                    subject: "BGP alert: " + message,
+                    text: "Hello world?"
+                })
+                .catch(error => {
+                    console.log("log the error properly!", error);
+                })
+        }
     }
 }
