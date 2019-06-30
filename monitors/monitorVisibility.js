c50e78dde7ebe200807041d21c63c0acb385c583
Massimo Candela <nve@webrobotics.net>
2019-06-30 13:31:05 +0200

    introduced visaibility monitoring

---
+++ monitors/monitorVisibility.js
@@ -0,0 +1,49 @@
+import Monitor from "./monitor";
+import ipUtils from "../ipUtils";
+import ip from "ip";
+
+export default class MonitorVisibility extends Monitor {
+
+    constructor(inputManager, name, channel, config, pubSub){
+        super(inputManager, name, channel, config, pubSub);
+    };
+
+    updateMonitoredPrefixes = () => {
+        this.monitored = this.input.getMonitoredMoreSpecifics();
+    };
+
+    filter = (message) => {
+        return message.type === 'announcement';
+    };
+
+    squashAlerts = (alerts) => {
+        return alerts[0].message;
+    };
+
+
+    monitor = (message) => new Promise((resolve, reject) => {
+
+        const messagePrefix = message.prefix;
+
+        let matches = this.monitored.filter(item => {
+            return item.prefix === messagePrefix;
+        });
+        if (matches.length > 1) {
+            matches = [matches.sort((a, b) => ipUtils.sortByPrefixLength(a.prefix, b.prefix)).pop()];
+        }
+
+        if (matches.length !== 0) {
+            const match = matches[0];
+            this.publishAlert(message.originAs + "-" + match.prefix,
+                `The prefix ${match.prefix} is announced by the AS${message.originAs} instead of AS${match.asn}`,
+                match.asn,
+                {
+                    rule: matches[0],
+                    received: message
+                });
+        }
+
+        resolve(true);
+    });
+
+}
