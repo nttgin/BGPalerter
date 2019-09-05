3b7f91490c771ca18f1ac0a146762ff4d7b5fdc7
Massimo Candela <nve@webrobotics.net>
2019-09-05 14:24:20 +0200

    remove build for windows, not supported yet

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ cd build
 
 yarn
 
-./node_modules/.bin/pkg . --targets node10-win-x64 --output ../bin/bgpalerter-win-x64
+#./node_modules/.bin/pkg . --targets node10-win-x64 --output ../bin/bgpalerter-win-x64
 
 ./node_modules/.bin/pkg . --targets node10-linux-x64 --output ../bin/bgpalerter-linux-x64
 
