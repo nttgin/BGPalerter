cf7951393f24d020040bbcfddf4d228bfa694800
Massimo Candela <massimo@us.ntt.net>
2020-01-21 01:38:10 +0100

    sync pkg and source code to node 12 LTS + enable build for windows

--- build.sh
+++ build.sh
@@ -13,11 +13,11 @@ cd build
 
 npm install --silent
 
-#./node_modules/.bin/pkg . --targets node10-win-x64 --output ../bin/bgpalerter-win-x64
+./node_modules/.bin/pkg . --targets node12-win-x64 --output ../bin/bgpalerter-win-x64
 
-./node_modules/.bin/pkg . --targets node10-linux-x64 --output ../bin/bgpalerter-linux-x64
+./node_modules/.bin/pkg . --targets node12-linux-x64 --output ../bin/bgpalerter-linux-x64
 
-./node_modules/.bin/pkg . --targets node10-macos-x64 --output ../bin/bgpalerter-macos-x64
+./node_modules/.bin/pkg . --targets node12-macos-x64 --output ../bin/bgpalerter-macos-x64
 
 cd ../
 rm -rf build
