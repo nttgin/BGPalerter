c88dea80bed90ca0125a4cca995220fcc45a8b61
Florian Domain <f.domain@criteo.com>
2020-05-01 22:38:21 +0200

    make soft update connector compatible with proxy

--- src/connectors/connectorSwUpdates.js
+++ src/connectors/connectorSwUpdates.js
@@ -38,6 +38,7 @@ export default class ConnectorSwUpdates extends Connector{
 
     constructor(name, params, env) {
         super(name, params, env);
+        axios.defaults.httpsAgent = env.agent;
     }
 
     connect = () =>
