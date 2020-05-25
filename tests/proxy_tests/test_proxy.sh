cdf98b74a75edc9a384303777c808dc72d930d62
Florian Domain <f.domain@criteo.com>
2020-05-25 15:56:49 +0200

    terminate anyproxy process once tests are done
    Anyproxy was still running in background once tests have finished.
    This changes allows to terminate properly the process by sending
    a SIGTERM.

--- tests/proxy_tests/test_proxy.sh
+++ tests/proxy_tests/test_proxy.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# Install anyproxy 
 npm install -g anyproxy
+# Run anyproxy in background and store its PID
 nohup anyproxy --port 8001 &
+ANYPROXY_PID=$!
+# Run tests suite
 ./node_modules/.bin/mocha --exit tests/proxy_tests/*.js --require @babel/register
+# Terminate anyproxy process
+kill $ANYPROXY_PID
+exit 0
