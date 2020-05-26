365078c39cb806969b5352b333c5aa2f82ec4be4
Massimo Candela <massimo@us.ntt.net>
2020-05-26 19:04:11 +0200

    improved build.sh

--- build.sh
+++ build.sh
@@ -3,17 +3,21 @@
 rm -rf bin
 mkdir bin
 
+rm -rf build
+mkdir build
+
+npm install
+
 npm run babel . --  --ignore node_modules --out-dir build
 
-cd build
+cp package.json build/package.json
 
-npm install --silent
+./node_modules/.bin/pkg ./build/package.json --targets node12-win-x64 --output bin/bgpalerter-win-x64 --loglevel=error
 
-./node_modules/.bin/pkg . --targets node12-win-x64 --output ../bin/bgpalerter-win-x64
+./node_modules/.bin/pkg ./build/package.json --targets node12-linux-x64 --output bin/bgpalerter-linux-x64 --loglevel=error
 
-./node_modules/.bin/pkg . --targets node12-linux-x64 --output ../bin/bgpalerter-linux-x64
+./node_modules/.bin/pkg ./build/package.json --targets node12-macos-x64 --output bin/bgpalerter-macos-x64 --loglevel=error
 
-./node_modules/.bin/pkg . --targets node12-macos-x64 --output ../bin/bgpalerter-macos-x64
+echo "--> BGPalerter compiled in bin/ (ignore the warnings about files that cannot be resolved)."
 
-cd ../
 rm -rf build
