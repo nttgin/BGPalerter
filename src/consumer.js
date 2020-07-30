2461cc5e73d6731290ecd4e55c9afe9fa57f9bca
Massimo Candela <massimo@us.ntt.net>
2020-07-30 14:57:52 +0200

    manage exceptions of corrupted report list

--- src/consumer.js
+++ src/consumer.js
@@ -41,12 +41,20 @@ export default class Consumer {
             this.connectors[connector.name] = connector.class
         }
 
-        this.monitors = env.config.monitors
-            .map(monitor => new monitor.class(monitor.name, monitor.channel, monitor.params || {}, env));
+        try {
 
-        this.reports = env.config.reports
-            .map(report => new report.class(report.channels, report.params || {}, env));
+            this.monitors = env.config.monitors
+                .map(monitor => new monitor.class(monitor.name, monitor.channel, monitor.params || {}, env));
 
+            this.reports = env.config.reports
+                .map(report => new report.class(report.channels, report.params || {}, env));
+
+        } catch (error) {
+            env.logger.log({
+                level: 'error',
+                message: error
+            });
+        }
         process.on('message', this.dispatch);
         env.pubSub.subscribe('data', (type, data) => {
             this.dispatch(data);
