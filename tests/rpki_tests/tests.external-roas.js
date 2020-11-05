4431dfa2ac589df40d5e5301567b9c49e597e0c8
Massimo Candela <massimo@us.ntt.net>
2020-11-05 15:59:24 +0100

    added test coverage for monitorROAS

---
+++ tests/rpki_tests/tests.external-roas.js
@@ -0,0 +1,102 @@
+/*
+ * 	BSD 3-Clause License
+ *
+ * Copyright (c) 2019, NTT Ltd.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+const chai = require("chai");
+const chaiSubset = require('chai-subset');
+const fs = require('fs');
+const expect = chai.expect;
+const asyncTimeout = 200000;
+chai.use(chaiSubset);
+
+global.EXTERNAL_CONFIG_FILE = "tests/rpki_tests/config.rpki.test.external-roas.yml";
+
+//Test rpki watch file reloading
+fs.copyFileSync("tests/rpki_tests/roas.before.json", "tests/rpki_tests/roas.json");
+
+setTimeout(() => {
+    //Test rpki watch file reloading
+    fs.copyFileSync("tests/rpki_tests/roas.after.json", "tests/rpki_tests/roas.json");
+}, 40000);
+
+const worker = require("../../index");
+const pubSub = worker.pubSub;
+
+describe("RPKI monitoring 4", function() {
+
+    it("ROA diff - external connector", function (done) {
+
+        const expectedData = {
+
+            "d6580b7dbaac973af25585b5a6e7bddc": {
+                id: 'd6580b7dbaac973af25585b5a6e7bddc',
+                truncated: false,
+                origin: 'rpki-monitor',
+                affected: 2914,
+                message: 'ROAs change detected: removed <2.3.4.0/24, 2914, 24, >'
+            }
+
+        };
+
+        let rpkiTestCompletedExternal = false;
+        pubSub.subscribe("rpki", function (type, message) {
+
+            if (!rpkiTestCompletedExternal) {
+                message = JSON.parse(JSON.stringify(message));
+                const id = message.id;
+
+                expect(Object.keys(expectedData).includes(id)).to.equal(true);
+                expect(expectedData[id] != null).to.equal(true);
+
+                expect(message).to.containSubset(expectedData[id]);
+
+                expect(message).to.contain
+                    .keys([
+                        "latest",
+                        "earliest"
+                    ]);
+
+                delete expectedData[id];
+                if (Object.keys(expectedData).length === 0) {
+                    setTimeout(() => {
+                        rpkiTestCompletedExternal = true;
+                        done();
+                    }, 80000);
+                }
+            }
+        });
+
+        setTimeout(() => { // Wait that the watcher realizes the file changed
+            pubSub.publish("test-type", "rpki");
+        }, 100000);
+
+    }).timeout(asyncTimeout);
+});
