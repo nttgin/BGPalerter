91c8e5858c9ade9be7935a158d945c4e3cc63772
Massimo Candela <massimo@us.ntt.net>
2022-12-25 20:02:28 +0100

    Revert "allow for repeated visibility alerts"
    This reverts commit 28af330edccb89fbc8e028a4f2fb340fc35ea3d7.

--- src/monitors/monitorVisibility.js
+++ src/monitors/monitorVisibility.js
@@ -76,7 +76,7 @@ export default class MonitorVisibility extends Monitor {
 
             if (matchedRule && !matchedRule.ignore && ipUtils._isEqualPrefix(matchedRule.prefix, messagePrefix)) {
 
-                const key = [parseInt(moment().unix() / 7200).toString(), matchedRule.prefix].join("-");
+                let key = matchedRule.prefix;
 
                 this.publishAlert(key,
                     matchedRule.asn.getId(),
