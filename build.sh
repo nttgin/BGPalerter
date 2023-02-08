6130e03c212e8c2a5d15e5c705e2efcb0da219ed
Massimo Candela <massimo@us.ntt.net>
2023-02-09 00:46:57 +0100

    migrate to node 18

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ npm ci --silent
 
 npm run compile
 
-./node_modules/.bin/pkg ./dist/package.json --targets node14-win-x64 --output bin/bgpalerter-win-x64 --loglevel=error
+./node_modules/.bin/pkg ./dist/package.json --targets node18-win-x64 --output bin/bgpalerter-win-x64 --loglevel=error
 
-./node_modules/.bin/pkg ./dist/package.json --targets node14-linux-x64 --output bin/bgpalerter-linux-x64 --loglevel=error
+./node_modules/.bin/pkg ./dist/package.json --targets node18-linux-x64 --output bin/bgpalerter-linux-x64 --loglevel=error
 
-./node_modules/.bin/pkg ./dist/package.json --targets node14-macos-x64 --output bin/bgpalerter-macos-x64 --loglevel=error
+./node_modules/.bin/pkg ./dist/package.json --targets node18-macos-x64 --output bin/bgpalerter-macos-x64 --loglevel=error
 
 echo "--> BGPalerter compiled in bin/ (ignore the warnings about files that cannot be resolved)."
 
